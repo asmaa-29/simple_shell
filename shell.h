28f15bd337a491f98d2168323f7d35d989ab7353
asma <asmaammar29@gmail.com>
2023-10-19 16:07:45 -0700

    simple shell project

--- shell.h
+++ shell.h
@@ -1,6 +1,50 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/*---MODE---*/
+void interactive_mode(char *progname);
+void non_interactive_mode(char *progname);
+
+/*---MACROS---*/
+extern char **environ;
+#define MAX_NUM 10
+#define MAX_LENGHT 256
+#define BUFFER_SIZE 1024
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/*---PROTOTYPES---*/
+char *read_stdin(void);
+char *read_stream(void);
+char **tokeniz(char *line);
+int excutcmd(char **cmd);
+void errors(char *s);
+char *_strdup(char *s);
+int _strlen(const char *s);
+int _strncmp(const char *str1, const char *str2, int n);
+char *_getenv(const char *name);
+void error(char *progname, int status, char **s, int running);
+void _itoa(int num, char *str);
+char *_strcat(char *dest, char *src);
+int end(char **cmd);
+char *_strcpy(char *dest, char *src);
+int env(char **cmd);
+char *_strtok(char *str, char *delim);
+int _getline(char **lineptr, size_t *n, FILE *stream);
+
+/*---STRUCTRES---*/
+/**
+ * struct built - structure for builtins.
+ * @name: name of the builtin.
+ * @f: function prototype.
+ */
+typedef struct built
+{
+	char *name;
+	int (*f)(char **);
+} built_in;
 #include <unistd.h>
 #include <limits.h>
 #include <fcntl.h>
